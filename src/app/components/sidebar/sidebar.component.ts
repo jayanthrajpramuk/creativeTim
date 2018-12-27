dbc109f8dde522b22c277c7b6ab601d866ed52ed
Jayanth <chornur.srinivasa.jayanth@volvo.com>
2018-12-27 11:31:50 +0530

    Base layout

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+ /*   { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'User Profile',  icon: 'person', class: '' },
+    { path: '/table-list', title: 'Table List',  icon: 'content_paste', class: '' },
+    { path: '/typography', title: 'Typography',  icon: 'library_books', class: '' },
+    { path: '/icons', title: 'Icons',  icon: 'bubble_chart', class: '' },
+    { path: '/maps', title: 'Maps',  icon: 'location_on', class: '' },
+    { path: '/notifications', title: 'Notifications',  icon: 'notifications', class: '' },*/
+    { path: '/registration', title: 'Registration',  icon: 'assignment', class: '' },
+    { path: '/appointment', title: 'Appointment',  icon: 'date_range', class: '' },
+    { path: '/billing', title: 'Bill',  icon: 'star', class: '' },
+    { path: '/observation', title: 'Observation',  icon: 'note_add', class: '' },
+    { path: '/report', title: 'Reports',  icon: 'bar_chart', class: '' },
+    { path: '/maintenence', title: 'Maintenence',  icon: 'control_point_duplicate', class: '' }/*,
+    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },*/
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
